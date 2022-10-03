9d84a37f6f48cc9086845ccee094995d1cea4836
Mette <mette.ghijsen@student.fontys.nl>
2022-10-03 22:23:02 +0200

    Button toegevoegd

--- app/src/main/java/com/example/helloworld/Activity2.kt
+++ app/src/main/java/com/example/helloworld/Activity2.kt
@@ -3,10 +3,9 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_2)
     }
 }
