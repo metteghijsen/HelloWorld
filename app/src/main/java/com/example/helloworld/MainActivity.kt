e5df28511fc66609c2303bbde5da0de7b7ebbdd4
Mette <481695@student.fontys.nl>
2022-10-04 10:14:08 +0200

    Knop toegevoegd naar nieuwe activiteit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.secondActivityBtn)
+        button.setOnClickListener {
+            //TODO:start activity
+            val intent = Intent(this, Activity2::class.java)
+// start your next activity
+            startActivity(intent)
+        }
     }
 }
